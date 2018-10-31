9a34c5e3a3fa42bb1af5573591c250a4dce55b2b
Brayden Killeen <18531092@students.latrobe.edu.au>
2018-10-31 13:54:28 +1100

    Eslint

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
-const { app, BrowserWindow } = require('electron')
+import { app, BrowserWindow } from 'electron';
 
 // Global reference of window object needed so that the window doesn't
 // get automatically closed with the JS object is garbage collected.
-let window
+let window;
 
 function createWindow() {
   // Create the browser window.
-  window = new BrowserWindow({ width: 800, height: 600 })
-  window.loadFile('index.html')
+  window = new BrowserWindow({ width: 800, height: 600 });
+  window.loadFile('index.html');
 
   // Opens DevTools
-  window.webContents.openDevTools()
+  // window.webContents.openDevTools()
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    mainWindow = null
-  })
+  window.on('closed', () => {
+    window = null;
+  });
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
-app.on('window-all-closed', function () {
+app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
-})
+});
 
-app.on('activate', function () {
+app.on('activate', () => {
   if (window == null) {
     createWindow();
   }
-})
+});
