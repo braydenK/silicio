503f69b510c5c801ae0a8ac47c98c5d8f52a86b5
Brayden Killeen <18531092@students.latrobe.edu.au>
2018-10-31 13:35:38 +1100

    Added functions for system events

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
 const { app, BrowserWindow } = require('electron')
 
+// Global reference of window object needed so that the window doesn't
+// get automatically closed with the JS object is garbage collected.
+let window
 
 function createWindow() {
   // Create the browser window.
   window = new BrowserWindow({ width: 800, height: 600 })
   window.loadFile('index.html')
+
+  // Opens DevTools
+  window.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
 }
 
 app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+})
+
+app.on('activate', function () {
+  if (window == null) {
+    createWindow();
+  }
+})
