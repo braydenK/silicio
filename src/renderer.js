5f4bb9cf57cf84acad9bb806a6e6322c47f8b32e
Brayden Killeen <18531092@students.latrobe.edu.au>
2018-11-02 00:04:20 +1100

    parsing unordered lists

--- src/renderer.js
+++ src/renderer.js
@@ -1,7 +1,6 @@
 document.getElementById('input').addEventListener('keyup', readInput);
 
 function readInput(event) {
-
   document.getElementById('output').innerHTML = parseLine();
 }
 
@@ -9,10 +8,10 @@ function parseLine() {
   let line = document.getElementById("input").value;
   let formattedLine= '';
 
-  console.log(line);
-
   if (line[0] === '#') {
     formattedLine = parseHeader(line);
+  } else if (line[0] === '-') {
+    formattedLine = parseUnorderedList(line);
   } else {
     formattedLine = line;
   }
@@ -32,4 +31,42 @@ function parseHeader(line) {
   }
 
   return `<h${headerSize}>${line.substring(1, line.length)}</${headerSize}>`;
+}
+
+// TODO: Very broken.
+function parseEmphasis(line) {
+  let emphasisSize;
+  let startIndex;
+  let endIndex;
+  let formattedString;
+
+  for (let i = 0; i < line.length; i++) {
+    if (line[i] === '*' && line[i+1] === '*') {
+      emphasisSize = 2;
+      if (startIndex === null) {
+        startIndex = i+emphasisSize;
+        i++;
+      } else {
+        endIndex = i;
+      }
+    } else if (line[i] === '*') {
+      emphasisSize = 1;
+      if (startIndex === null) {
+        startIndex = i + emphasisSize;
+        i++;
+      } else {
+        endIndex = i;
+      }
+    }
+  }
+
+  if (emphasisSize == 2) {
+    return `<b>${line.substring(startIndex, endIndex)}</b>`;
+  } else {
+    return `<em>${line.substring(startIndex, endIndex)}</em>`;
+  }
+}
+
+function parseUnorderedList(line) {
+  return `<li>${line.substring(1, line.length)}</li>`;
 }
